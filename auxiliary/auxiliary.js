0cb4c63364d4cd21530e0d5192e17ae4b9399158
Melanie Rodas <mrodasl@uni.pe>
2023-08-16 20:52:08 -0500

    added convertAbsPath and validateAbsolutePath

--- auxiliary/auxiliary.js
+++ auxiliary/auxiliary.js
@@ -12,10 +12,26 @@ function isValidPath(inputPath) {
   }
 }
 
+function convertToAbsPath(inputPath) {
+  return path.resolve(inputPath);
+}
+
+function validateAbsolutePath(inputPath) {
+  const normalizePath = path.normalize(inputPath);
+  if (path.isAbsolute(normalizePath)) {
+    return normalizePath;
+  } else {
+    return convertToAbsPath(normalizePath);
+  }
+}
 console.log("-------Example 0");
 console.log("isValidPath:", isValidPath("./noExiste"));
 console.log("-------Example 1");
 console.log(
   "isValidPath:",
   isValidPath("./../DEV008-md-links/testsMdLinks/folder/fileTwo.md")
+);
+console.log(
+  "validateAbsolutePath:",
+  validateAbsolutePath("./../DEV008-md-links/testsMdLinks/folder/fileTwo.md")
 );
