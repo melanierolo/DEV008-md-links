29c1bc403042502f095909aba3224c083ce1f88b
Melanie Rodas <mrodasl@uni.pe>
2023-08-16 20:11:50 -0500

    added a function called isValidPath

--- auxiliary/auxiliary.js
+++ auxiliary/auxiliary.js
@@ -1,8 +1,21 @@
 //  the file contains auxiliary functions
-console.log("Hello");
+const fs = require("fs");
+const path = require("path");
 
-function sum(a, b) {
-  return a + b;
+function isValidPath(inputPath) {
+  const normalizePath = path.normalize(inputPath);
+  if (fs.existsSync(normalizePath)) {
+    return true;
+  } else {
+    console.log("Path cannot be empty o doesn't exist");
+    return false;
+  }
 }
 
-console.log(sum(4, 5));
+console.log("-------Example 0");
+console.log("isValidPath:", isValidPath("./noExiste"));
+console.log("-------Example 1");
+console.log(
+  "isValidPath:",
+  isValidPath("./../DEV008-md-links/testsMdLinks/folder/fileTwo.md")
+);
