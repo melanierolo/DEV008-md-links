535f0371e7ec6e1b4753d34b060407e31f0001e4
Melanie Rodas <mrodasl@uni.pe>
2023-08-22 11:38:34 -0500

    changed the functions convertMarkdownToHtml and getLinksInHtmlFile

--- auxiliary/auxiliary.js
+++ auxiliary/auxiliary.js
@@ -67,11 +67,10 @@ console.log("-----Example 3--------:hasMdFileExtension");
 console.log("hasMdFileExtension", hasMdFileExtension(pathAbs));
 console.log("hasMdFileExtension", hasMdFileExtension(pathAbsTwo));
 console.log("-----Example 4--------:convertMarkdownToHTML");
-const fsPromise = require("fs").promises;
 
-async function convertMarkdownToHTML(pathAbs) {
+function convertMarkdownToHTML(pathAbs) {
   try {
-    const data = await fsPromise.readFile(pathAbs, { encoding: "utf8" });
+    const data = fs.readFileSync(pathAbs, { encoding: "utf8" });
     const fileHtml = marked.parse(data);
     return fileHtml;
   } catch (error) {
@@ -83,22 +82,16 @@ const pathAbsFileOne = validateAbsolutePath(
   "./../DEV008-md-links/testsMdLinks/file.md"
 );
 console.log("convertMarkdownToHTML");
-convertMarkdownToHTML(pathAbsFileOne)
-  .then((result) => {
-    console.log("example 4:", result);
-  })
-  .catch((error) => {
-    console.log(error);
-  });
+convertMarkdownToHTML(pathAbsFileOne);
 
 console.log("-----Example 5--------:getLinksInHtmlFile");
 const jsdom = require("jsdom");
 const { JSDOM } = jsdom;
 
-async function getLinksInHtmlFile(pathAbsFileOne) {
+function getLinksInHtmlFile(pathAbsFileOne) {
   try {
     let linksObjectsArray = [];
-    const htmlContent = await convertMarkdownToHTML(pathAbsFileOne);
+    const htmlContent = convertMarkdownToHTML(pathAbsFileOne);
     console.log("get HTML", htmlContent);
     const dom = new JSDOM(htmlContent, { includeNodeLocations: true }); // Include the option here
     const document = dom.window.document;
@@ -117,6 +110,4 @@ async function getLinksInHtmlFile(pathAbsFileOne) {
   }
 }
 
-getLinksInHtmlFile(pathAbsFileOne).then((document) => {
-  console.log("getLinksInHtmlFile-docu", document);
-});
+console.log(getLinksInHtmlFile(pathAbsFileOne));
