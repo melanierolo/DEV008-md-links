9ac2800344f0c9c1aac230bd8e7b7d590568b866
Melanie Rodas <mrodasl@uni.pe>
2023-09-01 17:27:48 -0500

    added color on the CLI

--- cli.js
+++ cli.js
@@ -1,6 +1,7 @@
 // CLI - show answers
 const { mdLinks } = require("./index.js");
 const yargs = require("yargs");
+const chalk = require("chalk");
 
 // Define the command using positional arguments and options
 yargs.command(
@@ -29,18 +30,50 @@ yargs.command(
   },
   (argv) => {
     const { path, validate, stats } = argv;
+    // Colors:
+    const colorBlueOne = chalk.hex("#4698F1");
+    const colorBlueTwo = chalk.hex("#0D6CE8");
+    const colorThree = chalk.hex("#703770");
+    const colorError = chalk.hex("#D62237");
+    const colorOk = chalk.hex("#58C94C");
+    const colorFail = chalk.hex("#E65558");
     console.log(path);
     // Call the mdLinks function with the provided path and options
     try {
       mdLinks(path, { validate: validate, stats: stats })
         .then((result) => {
-          console.log("result", result);
+          if (typeof result[0]["Total"] === "number") {
+            for (const [key, value] of Object.entries(result[0])) {
+              console.log(colorBlueOne.bold(`${key} : `) + `${value}`);
+            }
+          } else if (Object.values(result[0]).length === 3) {
+            result.forEach((element) => {
+              console.log(
+                `${element.file} ` +
+                  colorBlueTwo.italic(`${element.href}`) +
+                  colorThree(` ${element.text.slice(0, 50)}`)
+              );
+            });
+          } else {
+            result.forEach((element) => {
+              console.log(
+                `${element.file} ` +
+                  colorBlueOne.italic(`${element.href}`) +
+                  ` ${
+                    element.ok === "OK"
+                      ? colorOk.bold("OK" + ` ${element.status} `)
+                      : colorFail.bold("FAIL" + ` ${element.status} `)
+                  }` +
+                  colorThree(`${element.text.slice(0, 50)}`)
+              );
+            });
+          }
         })
         .catch((error) => {
-          console.log("Error:", error);
+          console.log(colorError.bold(error));
         });
     } catch (error) {
-      console.error(error);
+      console.log(colorError.bold(error));
     }
   }
 );
